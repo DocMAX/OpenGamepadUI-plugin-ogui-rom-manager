cca669c1fb1524d0befd58359ebef3480b033f1f
DocMAX <github@docmax.my.to>
2025-04-26 04:21:27 +0200

    changes

--- core/library.gd
+++ core/library.gd
@@ -7,13 +7,8 @@ func _ready() -> void:
 	logger = Log.get_logger("OguiRomManager", Log.LEVEL.INFO)
 	library_id = "ogui-rom-manager"
 	logger.info("OguiRomManager Library loaded with library_id: " + library_id)
-	
-	# Load the TabContainerState
 	tabs_state = load("res://core/ui/card_ui/library/library_tabs_state.tres") as TabContainerState
-	if tabs_state:
-		# Wait a frame to ensure all menus are ready
-		await get_tree().process_frame
-		_update_tabs()
+
 
 # Helper function to determine the depth and direction of ${title} in the glob
 func _get_title_depth(search_glob: String) -> Dictionary:
@@ -70,57 +65,15 @@ func _extract_title(file_path: String, depth: Dictionary, rom_dir: String) -> St
 		return title_parts[0].strip_edges()
 	return raw_title
 
-# Update tabs based on parser names
-func _update_tabs() -> void:
-	var settings_manager = load("res://core/global/settings_manager.tres") as SettingsManager
-	var parser_count = settings_manager.get_value("plugin.oguirommanager", "parser_count", 0)
-	
-	# Get existing tabs
-	var existing_tabs = tabs_state.tabs_text.duplicate()
-	var parser_tabs = []
-	
-	# Collect parser names
-	for i in range(parser_count):
-		var section = "plugin.oguirommanager.parser_" + str(i)
-		var parser_name = settings_manager.get_value(section, "name", "Unnamed Parser")
-		if parser_name != "Installed" and parser_name != "All Games":
-			parser_tabs.append(parser_name)
-	
-	# Remove tabs that no longer exist
-	for tab in existing_tabs:
-		if tab != "Installed" and tab != "All Games" and tab not in parser_tabs:
-			tabs_state.remove_tab(tab)
-	
-	# Add new parser tabs
-	for parser_name in parser_tabs:
-		if parser_name not in existing_tabs:
-			var tab_node = ScrollContainer.new()
-			tab_node.name = parser_name
-			tab_node.horizontal_scroll_mode = 0
-			var margin = MarginContainer.new()
-			margin.add_theme_constant_override("margin_left", 15)
-			margin.add_theme_constant_override("margin_top", 100)
-			margin.add_theme_constant_override("margin_right", 15)
-			margin.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-			margin.size_flags_vertical = Control.SIZE_EXPAND_FILL
-			var grid = HFlowContainer.new()
-			grid.name = parser_name + "Grid"
-			grid.add_theme_constant_override("h_separation", 26)
-			grid.add_theme_constant_override("v_separation", 16)
-			grid.alignment = HFlowContainer.ALIGNMENT_CENTER
-			grid.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-			margin.add_child(grid)
-			tab_node.add_child(margin)
-			tabs_state.add_tab(parser_name, tab_node)
-			logger.debug("Added parser tab: " + parser_name)
-	
-	# Ensure the tab container is updated before proceeding
-	await get_tree().process_frame
 
 func get_library_launch_items() -> Array[LibraryLaunchItem]:
 	var items: Array[LibraryLaunchItem] = []
 	var settings_manager = load("res://core/global/settings_manager.tres") as SettingsManager
 	var parser_count = settings_manager.get_value("plugin.oguirommanager", "parser_count", 0)
+
+	# Load tabs_state if not already loaded
+	if not tabs_state:
+		tabs_state = load("res://core/ui/card_ui/library/library_tabs_state.tres") as TabContainerState
 
 	for i in range(parser_count):
 		var section = "plugin.oguirommanager.parser_" + str(i)
@@ -129,6 +82,7 @@ func get_library_launch_items() -> Array[LibraryLaunchItem]:
 		var executable = settings_manager.get_value(section, "executable", "")
 		var parser_title = settings_manager.get_value(section, "name", "Unnamed Parser")
 		var titles_found = 0
+		var parser_items: Array[LibraryLaunchItem] = []
 
 		# Validate and parse the glob
 		if not "${title}" in search_glob:
@@ -162,7 +116,7 @@ func get_library_launch_items() -> Array[LibraryLaunchItem]:
 							item.args = [full_path]
 							item.tags = [parser_title]
 							item.installed = true
-							items.append(item)
+							parser_items.append(item)
 							titles_found += 1
 					filename = dir.get_next()
 				dir.list_dir_end()
@@ -209,11 +163,13 @@ func get_library_launch_items() -> Array[LibraryLaunchItem]:
 								item.args = [target_path]
 								item.tags = [parser_title]
 								item.installed = true
-								items.append(item)
+								parser_items.append(item)
 								titles_found += 1
 					filename = dir.get_next()
 				dir.list_dir_end()
 
+		# Add all items found for this parser to the main items list
+		items.append_array(parser_items)
 		logger.info("Parser '" + parser_title + "' loaded " + str(titles_found) + " titles")
 
 	return items
--- plugin.gd
+++ plugin.gd
@@ -2,12 +2,20 @@ extends Plugin
 
 var settings_menu := load("res://plugins/ogui-rom-manager/core/settings_menu.tscn") as PackedScene
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	logger = Log.get_logger("OGUIRM-Plugin", Log.LEVEL.DEBUG)
-	logger.info("OGUIRM plugin loaded")
 	var library: Library = load(plugin_base + "/core/library.tscn").instantiate()
-	add_child(library)
+	add_library(library)
+	logger.info("OGUIRM plugin loaded")
+#	call_deferred("_setup_plugin_tabs")
 
 func get_settings_menu() -> Control:
 	return settings_menu.instantiate()
+
+#func _setup_plugin_tabs() -> void:
+#	var tabs_state := load("res://core/ui/card_ui/library/library_tabs_state.tres") as TabContainerState
+#	var scene_resource := load("res://plugins/ogui-rom-manager/core/parser_tab.tscn") as PackedScene
+#	var my_category_tab_node := scene_resource.instantiate() as ScrollContainer
+#	my_category_tab_node.name = "MyCategory"
+#	tabs_state.add_tab("MyCategory", my_category_tab_node)
